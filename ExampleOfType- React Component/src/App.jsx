b5a88a5554420866cb21fcad63086d72a9c6f478
Ahmed-Al-Badri <albadri@pdx.edu>
2025-05-06 10:18:34 -0700

    first commit

---
+++ ExampleOfType- React Component/src/App.jsx
@@ -0,0 +1,11 @@
+import Example from "./Example/Example";
+
+function App() {
+  return (
+    <>
+      <Example />
+    </>
+  );
+}
+
+export default App;
---
+++ ExampleOfType- React Component/src/Example/Example.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export default class Example extends Component {
+  constructor(probs) {
+    super(probs);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello, this is an example</p>
+      </div>
+    );
+  }
+}
